cf22fa28b802a7be28f70689b876a5f939d92e32
isaacwatson30 <isaacwatson30@gmail.com>
2025-11-23 21:03:49 -0600

    added index.js and src folder, also public folder with it files

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello Express!</h1><p>Your server is working!</p>");
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
